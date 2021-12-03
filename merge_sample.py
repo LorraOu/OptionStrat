3ce90e9216f0cb9095e642ea565659466e0c0be3
歐雅婷 <lorraou@Moonstone.local>
2021-12-03 22:19:27 +0800

    update

--- merge_sample.py
+++ merge_sample.py
@@ -139,7 +139,7 @@ if __name__ == '__main__':
                     if opt in f:
                         if count > 3:
                             count = 0
-                            continue
+                            break
                         if os.path.isfile(in_path + f'/option_codes/{opt}.csv'):
                             option_df = pd.read_csv(in_path + f'/option_codes/{opt}.csv')
                             option_df = option_df.set_index('Code')
