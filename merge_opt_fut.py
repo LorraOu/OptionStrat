398931841913a2dd9ead7851adee0724a2bbef2e
歐雅婷 <lorraou@Moonstone.local>
2021-12-06 23:57:41 +0800

    update

--- merge_opt_fut.py
+++ merge_opt_fut.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
                         opt_df = pd.read_csv(opt_path + f'/{date}/{opt_code}.csv')
                         opt_df = opt_df[opt_df['Tick']!=0]
                         # 如果之前有做過就跳過
-                        if os.path.isfile(f'/home/user/NasPublic/Option_Data/Price/{opt_code}_{date[0:4]}-{date[4:6]}-{date[6:8]}.csv'):
+                        if os.path.isdir(f'/home/user/NasPublic/Option_Data/Price/{date}'):
                             continue
                         # 只做到期前一個禮拜的資料
                         if len(opt_df) == 0:
